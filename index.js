a161d3be0079c3d61ca77b0d7b0d27ce70d12ce5
Glory-Lyka Okposin <lykaedem11@gmail.com>
2024-02-16 05:15:12 +0100

    updated

--- index.js
+++ index.js
@@ -1,14 +1,102 @@
 const http = require("http");
-const behaviour = require("./action.js");
+const PORT = 5050;
+const {
+  getBooks,
+  postBooks,
+  patchBooks,
+  putBooks,
+  deleteBooks,
+} = require("./booksController");
+const {
+  getAuthors,
+  postAuthor,
+  putAuthors,
+  patchAuthors,
+  deleteAuthors,
+} = require("./authorsController");
+const { findUser } = require("./db.user");
 
-const PORT = 9000;
+function getBodyFromString(req) {
+  return new Promise((resolve, reject) => {
+    const data = [];
+    req.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    req.on("end", () => {
+      const body = Buffer.concat(data).toString();
+      if (body) {
+        // in case the body is a JSON object
+        resolve(JSON.parse(body));
+        return;
+      }
+      resolve({});
+    });
+    req.on("error", (err) => {
+      reject(err);
+    });
+  });
+}
 
-// console.log(today.getfullYear());
+function authenticate(req, res, next) {
+  const { username, password } = req.body;
+  console.log("authenticate", req.body);
+  const user = findUser(username);
+  if (!user) {
+    res.statusCode = 403;
+    res.end();
+    return;
+  }
+  if (user.username !== username || user.password !== password) {
+    res.statusCode = 403;
+    res.end();
+    return;
+  }
+  next(req, res);
+}
 
-const server = http.createServer(behaviour);
+const server = http.createServer(async (req, res) => {
+  // console.log(generateRandomString(20));
+  try {
+    const body = await getBodyFromString(req);
+    req.body = body;
+    // booksController endpoints
+    if (req.url === "/books" && req.method === "GET") {
+      authenticate(req, res, getBooks);
+    }
+    if (req.url === "/books" && req.method === "POST") {
+      postBooks(req, res);
+    }
+    if (req.url === "/books" && req.method === "PUT") {
+      putBooks(req, res);
+    }
+    if (req.url === "/books" && req.method === "PATCH") {
+      patchBooks(req, res);
+    }
+    if (req.url === "/books" && req.method === "DELETE") {
+      deleteBooks(req, res);
+    }
+    // authorControllers
+    if (req.url === "/authors" && req.method === "GET") {
+      authenticate(req, res, getAuthors);
+    }
+    if (req.url === "/authors" && req.method === "POST") {
+      postAuthor(req, res);
+    }
+    if (req.url === "/authors" && req.method === "PUT") {
+      putAuthors(req, res);
+    }
+    if (req.url === "/authors" && req.method === "PATCH") {
+      patchAuthors(req, res);
+    }
+    if (req.url === "/authors" && req.method === "DELETE") {
+      deleteAuthors(req, res);
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.end(error.message);
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`);
 });
-
-// builtinModules.exoport;
